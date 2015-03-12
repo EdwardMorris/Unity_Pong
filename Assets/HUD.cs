66ae9d38e76df6cadd032069d4d1f308b4db130f
Edward <18edwardmorris@gmail.com>
2015-03-12 19:14:08 -0400

    Initial Commit

---
+++ Assets/HUD.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+	
+	public GameObject Player1;
+	public GameObject Player2;
+
+	public GameObject topWall;
+	public GameObject botWall;
+	public GameObject leftWall;
+	public GameObject rightWall;
+
+}
